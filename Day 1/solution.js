a167f4a84a3d891081dceb9e7e1d08dfd4a4230c
Tayden Flitcroft <taydengoodeill@gmail.com>
2024-12-02 01:00:02 -0600

    day 1 solution

---
+++ Day 1/solution.js
@@ -0,0 +1,38 @@
+const { readFileSync } = require('fs')
+const path = require('path')
+
+const input = readFileSync(path.join(__dirname, 'input.txt'), 'utf-8')
+	.split('\n')
+
+const [ left, right ] = input.reduce((acc, val) => {
+	const [ leftVal, rightVal ] = val.split(/\s+/).map(Number)
+	acc[0].push(leftVal)
+	acc[1].push(rightVal)
+	return acc
+}, [[], []])
+
+const quantityInRightList = left.reduce((acc, val) => {
+	acc[val] = right.filter(value => value === val).length
+	return acc
+}, {})
+
+left.sort((a, b) => a - b)
+right.sort((a, b) => a - b)
+
+const getDistance = (firstVal, secondVal) => {
+	return Math.abs(firstVal - secondVal)
+}
+
+let part1 = 0
+let part2 = 0
+
+for (let i = 0; i < left.length; i++) {
+	const currentLeft = left[i]
+	const currentRight = right[i]
+
+	part1 += getDistance(currentLeft, currentRight)
+	part2 += currentLeft * quantityInRightList[currentLeft]
+}
+
+console.log(part1)
+console.log(part2)
